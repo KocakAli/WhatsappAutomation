7980d20a1827f47a1780be1e7f31f5e37f468c37
Ali Koçak <kocakali5834@gmail.com>
2022-09-15 12:18:53 +0300

    Update script.py

--- script.py
+++ script.py
@@ -8,25 +8,28 @@ import pandas
 from selenium.webdriver.common.keys import Keys
 
 
-excel_data = pandas.read_excel('data.xlsx', sheet_name='data')
+excel_data = pandas.read_excel('data.xlsx', sheet_name='data') // first argument your workbook name and second argument your worksheet name
 
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+
 driver.get('https://web.whatsapp.com')
-input("Qr kod ile giriş yaptıktan sonra devam etmek için ENTER tuşuna basın.")
+count = 1
+input("Press Enter after you logged in with QR code.")
 for column in excel_data['Contact'].tolist():
     try:
-        url = 'https://web.whatsapp.com/send?phone=' + str(excel_data['Contact'][count]) + '&text=' + excel_data['Message'][0]  
+        url = 'https://web.whatsapp.com/send?phone=' + str(excel_data['Contact'][count]) + '&text=' + excel_data['Message'][0]
         driver.get(url)
         xpath_val = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
         wait = WebDriverWait(driver, 10)
         sleep(2)
         wait.until(EC.presence_of_element_located((By.XPATH, xpath_val))).send_keys(Keys.ENTER)
         sleep(5)
-        print('Mesaj gönderildi: ' + str(excel_data['Contact'][count]))
+        print('Message sent successfully: ' + str(excel_data['Contact'][count]))
     except Exception as e:
-        print('Mesaj gönderilemedi: ' + str(excel_data['Contact'][count]) + str(e))
+        print('Message can not sent: ' + str(excel_data['Contact'][count]) + str(e))
+    count = count + 1
 driver.quit()
-print("Script başarıyla çalıştırıldı.")
+print("Script has done.")
 
